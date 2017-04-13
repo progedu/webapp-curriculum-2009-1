3ccba51ca618500c74ae92afc0b1018a87b32341
murosan <masashi_16n1200013@nnn.ed.jp>
2017-04-13 17:17:17 +0900

    挑戦の初級

--- src/test/scala-2.11/NumberFormatterSpec.scala
+++ src/test/scala-2.11/NumberFormatterSpec.scala
@@ -2,5 +2,10 @@ import org.scalatest._
 
 class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
 
+  "formatメソッド" should "数値に3桁ごとのカンマ区切りを入れる" in {
+    assert(NumberFormatter.format(123123123) === "123,123,123")
+    assert(NumberFormatter.format(-123123123) === "-123,123,123")
+  }
+
 }
 
