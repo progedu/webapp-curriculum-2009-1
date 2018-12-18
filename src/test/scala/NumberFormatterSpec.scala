a7805012b03ea403d97440bde0552505ad330176
Maataro <masahiro.infinite77@gmail.com>
2018-12-18 17:15:44 +0900

    Update NumberFormatterSpec.scala

--- src/test/scala/NumberFormatterSpec.scala
+++ src/test/scala/NumberFormatterSpec.scala
@@ -5,7 +5,7 @@ class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
   "format関数" should "整数を取得し、数値を 3 桁ごとにカンマ区切りを入れる事ができる" in {
     assert(NumberFormatter.format(1000) === "1,000")
     assert(NumberFormatter.format(1000000) == "1,000,000")
-    assert(NumberFormatter.format(100) == 100)   // エラー: ,100 と表示された
+    assert(NumberFormatter.format(100) == "100")   // エラー: ,100 と表示された
   }
 
 }
