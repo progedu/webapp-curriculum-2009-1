2f83f50a41fd691ae567f793752ca7ad0698eed8
208-209 <208_209@outlook.jp>
2019-03-20 18:23:31 +0900

    format関数とテストの実装

--- src/main/scala/NumberFormatter.scala
+++ src/main/scala/NumberFormatter.scala
@@ -1,16 +1,18 @@
 object NumberFormatter {
 
   def format(number: Int): String = {
-    val reversed: String = number.toString.reverse
-    val indexedSeq: Seq[(Char, Int)] = reversed.zipWithIndex
-    val nestedSeq: Seq[Seq[Char]] = indexedSeq.map(
-      (t) => if (t._2 % 3 == 2) {
-        Seq(t._1, ',')
-      } else {
-        Seq(t._1)
-      })
-    nestedSeq.flatten.reverse.mkString
-  }
 
+    def f(string: String): String = {
+      val reversed: String = string.reverse
+      val indexedSeq: Seq[(Char, Int)] = reversed.zipWithIndex
+      val nestedSeq: Seq[Seq[Char]] = indexedSeq.map { case (c, i) =>
+        if (i % 3 == 2 && i != string.length - 1) Seq(c, ',')
+        else Seq(c)
+      }
+      nestedSeq.flatten.reverse.mkString
+    }
+
+    if (number < 0) "-" + f(number.toString.tail) else f(number.toString)
+  }
 }
 
--- src/test/scala/NumberFormatterSpec.scala
+++ src/test/scala/NumberFormatterSpec.scala
@@ -1,6 +1,20 @@
 import org.scalatest._
+import NumberFormatter._
 
 class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
+
+  "format関数" should "数値を 3 桁ごとにカンマ区切りを入れることができる" in {
+    assert(format(1) === "1")
+    assert(format(12) === "12")
+    assert(format(123) === "123")
+    assert(format(1234) === "1,234")
+    assert(format(0) === "0")
+    assert(format(-1) === "-1")
+    assert(format(-12) === "-12")
+    assert(format(-123) === "-123")
+    assert(format(-1234) === "-1,234")
+
+  }
 
 }
 
