085b10f1d7dfe2b5da111763375b2cc9831c1ab7
sdk40010 <sdk40010@gmail.com>
2019-04-06 00:17:54 +0900

    NumberFormatterの不具合を修正

--- src/test/scala/NumberFormatterSpec.scala
+++ src/test/scala/NumberFormatterSpec.scala
@@ -1,6 +1,12 @@
 import org.scalatest._
 
 class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
+  val nf = NumberFormatter
 
+  "format関数" should "数値を３行ごとにカンマ区切りを入れる" in {
+    assert(nf.format(100) == "100")
+    assert(nf.format(1000) == "1,000")
+    assert(nf.format(-100000) == "-100,000")
+  }
 }
 
