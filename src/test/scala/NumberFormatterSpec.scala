f30330fa0aad4562014f861310a2f3edef3af245
Taiyo Utsuhara <33287507+taiyoutsuhara@users.noreply.github.com>
2019-05-19 21:32:39 +0900

    NumberFormatterのテストとデバッグ

--- src/test/scala/NumberFormatterSpec.scala
+++ src/test/scala/NumberFormatterSpec.scala
@@ -1,6 +1,15 @@
 import org.scalatest._
 
 class NumberFormatterSpec extends FlatSpec with DiagrammedAssertions {
-
+  it should "NumberFormat" in {
+    import NumberFormatter._
+    assert(format(1) === "1")
+    assert(format(0) === "0")
+    assert(format(-1) === "-1")
+    assert(format(100) === "100")
+    assert(format(-100) === "-100")
+    assert(format(1000) === "1,000")
+    assert(format(-1000) === "-1,000")
+  }
 }
 
