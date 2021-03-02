fd8624c02b83d615a2406a400143b8e506f68ef9
yrberrytea <yrberrytea@gmail.com>
2021-03-02 17:47:07 +0900

    NumberFormatterの不具合を修正

--- src/main/scala/NumberFormatter.scala
+++ src/main/scala/NumberFormatter.scala
@@ -1,16 +1,16 @@
 object NumberFormatter {
 
   def format(number: Int): String = {
-    val reversed: String = number.toString.reverse
+    val minus = if (number < 0) "-"  else ""
+    val reversed: String = number.abs.toString.reverse
     val indexedSeq: Seq[(Char, Int)] = reversed.zipWithIndex
     val nestedSeq: Seq[Seq[Char]] = indexedSeq.map(
-      (t) => if (t._2 % 3 == 2) {
+      (t) => if ((t._2 % 3 == 2) && !(t._2 + 1 == indexedSeq.length)) {
         Seq(t._1, ',')
       } else {
         Seq(t._1)
       })
-    nestedSeq.flatten.reverse.mkString
+    minus + nestedSeq.flatten.reverse.mkString
   }
 
 }
-
